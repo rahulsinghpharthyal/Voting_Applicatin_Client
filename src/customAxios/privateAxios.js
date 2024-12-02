4568eb6880d261fbb06cc5d3a77e26aa6b7a6cb0
Rahul Pharthyal <rahulpharthyal04@gmail.com>
2024-12-02 23:02:17 +0530

    changes the backend url change the hosted URL in this

--- src/customAxios/privateAxios.js
+++ src/customAxios/privateAxios.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
-export const BASE_URL = 'http://localhost:7000'
+
+
+// export const BASE_URL = 'http://localhost:7000'
+export const BASE_URL = https://voting-application-server.onrender.com
 
 export default axios.create({
     baseURL: BASE_URL,
