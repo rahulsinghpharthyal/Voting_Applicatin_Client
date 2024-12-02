bef2675dd4d1237bcd2a0a5187cecb56ff5090cd
Rahul Pharthyal <rahulpharthyal04@gmail.com>
2024-12-02 23:27:18 +0530

    change favicon and other option details

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { handleLogout } from '../common/config/handleLogout';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 const Header = () => {
   const dispatch = useDispatch();
+  const {user} = useSelector((state)=>state.auth);
   return (
     <header className="bg-gray-800 text-white p-4 shadow-lg m-2 rounded-lg">
       <div className="container mx-auto flex justify-between items-center">
-        <div className="text-2xl font-bold">Welcome Rahul! Voter Profile</div>
+        <div className="text-2xl font-bold">Welcome {user?.username} in your Voter Profile!</div>
         <nav className="flex space-x-4">
           <Link to="/voter" className="hover:border-b-2 px-3 py-2 rounded">
             VoteToCandidate
